cc255303c5b7bd74977138c47b17ae1f6fb51a90
tejasvi raj <tejasviraj8686@gmail.com>
2024-02-09 17:44:06 +0530

    shopping_cart

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux"; 
+import { store } from './redux/Store.js';
+import { ToastContainer } from "react-toastify";
+import { Toaster } from "react-hot-toast";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+  <Provider store={store}>
+    <App />   
+    <Toaster/>  
+  </Provider>
+</BrowserRouter>
+   
+
+)
